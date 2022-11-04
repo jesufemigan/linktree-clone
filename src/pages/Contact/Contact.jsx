067c9985125d19642aae7fc9201d251dfa273f57
ElpidaHope <paul.oladapo@yahoo.com.com>
2022-11-04 22:21:33 +0100

    feat: error message to textarea

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,14 +1,36 @@
 import Footer from '../../components/Footer';
 import './contact.css';
 
+import { useRef, useState } from 'react';
+
 const Contact = () => {
+  const name = 'Jesufemi'
+
+  const messageRef = useRef(null)
+  const [messageToSend, setMessageToSend] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (messageToSend === '') {
+      messageRef.current.classList.add('error')
+    }
+    messageRef.current.nextSibling.style.display = 'block'
+
+  }
+
+  if (messageToSend !== '') {
+    messageRef.current.classList.remove('error')
+    messageRef.current.nextSibling.style.display = 'none'
+  }
+  
   return (
     <>
       <div className='contact-container'>
         <h1>Contact Me</h1>
         <p>Hi there, contact me to ask about anything you have in mind.</p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='name'>
             <div className='input'>
               <label htmlFor="first_name">First name</label>
@@ -25,14 +47,15 @@ const Contact = () => {
           </div>
           <div className='input'>
             <label htmlFor="message">Message</label>
-            <textarea name="" id="message" placeholder="Send me a message and I'll reply as soon as possible" />
+            <textarea id="message" placeholder="Send me a message and I'll reply as soon as possible" ref={messageRef} value={messageToSend} onChange={(e) => setMessageToSend(e.target.value)}/>
+            <small>Please enter a message</small>
           </div>
           <div className='checkbox-container'>
           <label className="checkbox">
             <input type="checkbox" />
             <span className="checkmark"></span>
           </label>
-            <p>You agree to providing your data to Jesufemi who may contact you.</p>
+            <p>You agree to providing your data to {name} who may contact you.</p>
           </div>
 
           <button type='submit' id='btn__submit'>Send Message</button>
